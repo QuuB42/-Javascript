c2feace135c9a50853a84d4bd65e1ffd963fc9be
Bogdan Gromskiy <creation01910@gmail.com>
2022-05-14 15:20:07 +0300

    Перша практика

---
+++ app.js
@@ -0,0 +1,5 @@
+const answers = []
+ answers[0] = prompt ("what is your name?",'');
+ answers [1] = prompt ("what is your somane?",'');
+ answers [2] = prompt ("what is your age?",'');
+document.write (answers);
